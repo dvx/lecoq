236c18e0e543ac1b8efccc9d2b3b907345269731
David Titarenco <david.titarenco@gmail.com>
2014-07-07 21:45:44 -0700

    fixing some spacing

--- app/controllers/Lecoq.java
+++ app/controllers/Lecoq.java
@@ -16,33 +16,33 @@ public class Lecoq extends Controller {
 	public static Result index() {
 		return ok(index.render());
 	}
-	
+
 	// this does all the work
 	public static Result service(String url) {
 		try {
 			// this is really messy. I used to like Play, but not so much recently :/
-	        Promise<Result> res = WS
-	                .url(url).head()
-	                .map(new Function<WS.Response, Result>() {
-	                    public Result apply(WS.Response response) {
+			Promise<Result> res = WS
+					.url(url).head()
+					.map(new Function<WS.Response, Result>() {
+						public Result apply(WS.Response response) {
 							ObjectNode result = Json.newObject();
 							result.put("status", "OK");
 							// assuming the server is being honest...
 							result.put("type", response.getHeader(CONTENT_TYPE));
 							return ok(result);
-	                        }
-	                    }
-	                );
-	        
+						}
+					}
+				);
+
 	        // for that CORS goodness
 			Context.current().response().setHeader("Access-Control-Allow-Origin", "*");
-			
+
 			// don't waste server resources, 5 seconds to reach your destination
-	        return res.getWrappedPromise().await(5, java.util.concurrent.TimeUnit.SECONDS).get();
+			return res.getWrappedPromise().await(5, java.util.concurrent.TimeUnit.SECONDS).get();
 		}
-        // need to use pokemon pattern here (gotta' catch'em all)
-        // in reality, we need TimeoutException, IllegalArgumentException, and a bunch of others
-        // but Promises are fucked in Play 2.0.x and exceptions don't trickle down
+		// need to use pokemon pattern here (gotta' catch'em all)
+		// in reality, we need TimeoutException, IllegalArgumentException, and a bunch of others
+		// but Promises are fucked in Play 2.0.x and exceptions don't trickle down
 		catch (Exception e) {
 			ObjectNode result = Json.newObject();
 			result.put("status", "KO");
