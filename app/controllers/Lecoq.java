68a1d26b9cb7536dea3d0a17bb5364e2f579f1e0
David Titarenco <david.titarenco@gmail.com>
2014-07-07 21:27:52 -0700

    final product (more or less)

--- app/controllers/Lecoq.java
+++ app/controllers/Lecoq.java
@@ -12,27 +12,38 @@ import views.html.*;
 
 public class Lecoq extends Controller {
 
+	// goto index/API doc
 	public static Result index() {
 		return ok(index.render());
 	}
-
+	
+	// this does all the work
 	public static Result service(String url) {
 		try {
+			// this is really messy. I used to like Play, but not so much recently :/
 	        Promise<Result> res = WS
 	                .url(url).head()
 	                .map(new Function<WS.Response, Result>() {
 	                    public Result apply(WS.Response response) {
 							ObjectNode result = Json.newObject();
 							result.put("status", "OK");
+							// assuming the server is being honest...
 							result.put("type", response.getHeader(CONTENT_TYPE));
 							return ok(result);
 	                        }
 	                    }
-	                );			
+	                );
 	        
+	        // for that CORS goodness
 			Context.current().response().setHeader("Access-Control-Allow-Origin", "*");
-	        return res.getWrappedPromise().await().get();
-		} catch (Exception e) {
+			
+			// don't waste server resources, 5 seconds to reach your destination
+	        return res.getWrappedPromise().await(5, java.util.concurrent.TimeUnit.SECONDS).get();
+		}
+        // need to use pokemon pattern here (gotta' catch'em all)
+        // in reality, we need TimeoutException, IllegalArgumentException, and a bunch of others
+        // but Promises are fucked in Play 2.0.x and exceptions don't trickle down
+		catch (Exception e) {
 			ObjectNode result = Json.newObject();
 			result.put("status", "KO");
 			result.put("err", e.getMessage());
